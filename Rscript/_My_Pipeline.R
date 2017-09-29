290ff413cb7e6a4c1187e1fc8ba9a12658462546
Peng Lin <penglin03@gmail.com>
2017-09-29 16:33:14 -0700

    Patch our test workflow.

--- Rscript/_My_Pipeline.R
+++ Rscript/_My_Pipeline.R
@@ -5,17 +5,16 @@ gc()
 ### Find true capital-state pairs from all possible capital-state pairs
 
 # ---- GBSERVER API ----
-source("./experimentAPI.R")
+source("./_My_ExperimentAPI.R")
 
 # ---- INPUT and CONFIGURATIONS ----
 
 EDGE_TYPE_FILE = "../data/infobox.edgetypes" # Example : "../data/lobbyist.edgetypes"
-INPUT_FILE = "../facts/state_capital2.csv" # Example : "../facts/lobbyist/firm_payee.csv" col 1 and 2 are ids and 3 is label
-CLUSTER_SIZE = 48 # Number of workers in gbserver
-FALSE_PER_TRUE = 5
-DISCARD_REL = 191
-ASSOCIATE_REL = c(404)
+## INPUT_FILE = "../facts/state_capital2.csv" # Example : "../facts/lobbyist/firm_payee.csv" col 1 and 2 are ids and 3 is label
+CLUSTER_SIZE = 4 # Number of workers in gbserver
 max_depth = 3
+
+DISCARD_REL = 191
 
 # ---- Load edge type file ----
 
@@ -33,53 +32,61 @@ clusterExport(cl = cl, varlist=c("adamic_adar", "semantic_proximity", "ppagerank
                                  "as.numeric", "request","DISCARD_REL"), envir = environment())
 
 # ---- Load input data ----
-dat.true <- read.csv(INPUT_FILE)
+## dat <- read.csv(INPUT_FILE)
 
-if (ncol(dat.true) < 3)
-  dat.true$label <- T
+relation.list <- read.csv("../data/gfc_input_relations.tsv", head=FALSE, sep="\t")
+colnames(relation.list) <- c("relation", "discard_rel")
+
+relation.list
+
+for (i in 1:nrow(relation.list)) {
+  rel.str <- relation.list[i, 1]
+  rel.int <- relation.list[i, 2]
+  train <- paste("../data/groundtruth/", rel.str, "_train.tsv", sep="")
+  test <- paste("../data/groundtruth/", rel.str, "_test.tsv", sep="")
+
+  datatrain <- read.csv(train, sep="\t")
+  datatest <- read.csv(test, sep="\t")
+
+  colnames(datatrain) <- c("src","dst","label")
+  colnames(datatest) <- c("src","dst","label")
+
+  # or use full paths: extract.fullpaths(datatrain, rel.int)
+  featuremerged <- extract.predicatepaths(rbind(datatrain, datatest))
+
+  featurestrain <- featuremerged[1:nrow(datatrain), ]
+  featurestest <- featuremerged[(nrow(datatrain) + 1):nrow(featuremerged), ]
+
+  write.csv(featurestrain, "train.csv")
+  write.csv(featurestest, "test.csv")
+
+  modeltrain <- Logistic(label~.,featurestrain)
+  evaltest <- evaluate_Weka_classifier(modeltrain, newdata=featurestest, cost=NULL, numFolds = 0, complexity = T, class = T, seed = NULL)
+
+  print(evaltest)
+}
 
 # ---- Construct false labeled data -----
-set.seed(233)
 
-# # TODO: reformat this so it is universal and file independent
-# dat.false <- rbind.fill(apply(dat.true, 1, function(x){
-#   candidates <- unique(dat.true[which(dat.true[,1] != x[1]), 2])
-#   candidates <- unlist(lapply(candidates, function(y){
-#     if(length(which(dat.true[,1] == x[1] & dat.true[,2] == y) != 0)) {
-#       return(NULL)
-#     }
-#     return(y)
-#   }))
-#   return(data.frame(src=x[1], 
-#                     dst=sample(candidates, FALSE_PER_TRUE),
-#                     label=F))
-# }))
-# 
-# colnames(dat.true) <- c("src","dst","label")
-# dat <- rbind(dat.true, dat.false)
-dat <- dat.true
-
-dat
-
-elapsed.time <- data.frame()
+# elapsed.time <- data.frame()
 
 ## Test Method
 
-experiment.fullpath.test <- eval.fullpath.test(dat, DISCARD_REL)
-write.csv(experiment.fullpath.test$raw, "../result/city/capital_state_all.fullpath.test.csv", row.names=F)
+# experiment.fullpath.test <- eval.fullpath.test(dat, DISCARD_REL)
+# write.csv(experiment.fullpath.test$raw, "../result/city/capital_state_all.fullpath.test.csv", row.names=F)
+# 
+# print("FULL PATH")
+# print(experiment.fullpath.test$eval)
+# 
+# elapsed.time <- rbind(elapsed.time, data.frame(method="fullpath.test", 
+#                                                elapsed = experiment.fullpath.test$elapsed[3] * CLUSTER_SIZE / nrow(dat)))
 
-print("FULL PATH")
-print(experiment.fullpath.test$eval)
+## experiment.test <- eval.test(dat, DISCARD_REL)
+## write.csv(experiment.test$raw, "../result/city/capital_state_all.test.csv", row.names=F)
+## print("PREDICATE PATH")
+## print(experiment.test$eval)
 
-elapsed.time <- rbind(elapsed.time, data.frame(method="fullpath.test", 
-                                               elapsed = experiment.fullpath.test$elapsed[3] * CLUSTER_SIZE / nrow(dat)))
-
-experiment.test <- eval.test(dat, DISCARD_REL)
-write.csv(experiment.test$raw, "../result/city/capital_state_all.test.csv", row.names=F)
-print("PREDICATE PATH")
-print(experiment.test$eval)
-
-elapsed.time <- rbind(elapsed.time, data.frame(method="test", 
-                                               elapsed = experiment.test$elapsed[3] * CLUSTER_SIZE / nrow(dat)))
+## elapsed.time <- rbind(elapsed.time, data.frame(method="test", 
+##                                                elapsed = experiment.test$elapsed[3] * CLUSTER_SIZE / nrow(dat)))
 
 q()
